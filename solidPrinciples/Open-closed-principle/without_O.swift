f28819b4447096d70ea4bf930c6f6b8d985d6d07
Erhan <demirci.erhan@windowslive.com>
2022-03-10 14:29:46 +0300

    open-closed principles completed

--- solidPrinciples/Open-closed-principle/without_O.swift
+++ solidPrinciples/Open-closed-principle/without_O.swift
@@ -7,23 +7,69 @@
 
 import UIKit
 
+
+/*
+ This program does not follow OSP(Open Closed Principles)  because Area() is not open to extension and can only ever handle Rectangle and Circle shapes. If we want to add support for Triangle, we’d have to modify the method, so it is not closed to modification.
+ */
+
+
 class without_O: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var a=shape()
+        a.whichShape="rectangle"
+        a.width=10
+        a.height=10
+        
+        var b=shape()
+        b.whichShape="circle"
+        b.radious=3
+        
+        print(SumArea(shapes: [a,b]))
+    }
+    
+    // Does not follow OSP
+    fileprivate func SumArea(shapes:[shape])->Double
+    {
+        var area=0.0
+        for  shape in shapes
+        {
+            if (shape.whichShape=="rectangle")
+            {
+               
+                area += shape.width*shape.height;
+            }
+            else if(shape.whichShape=="circle")
+            {
+            
+                area += shape.radious * shape.radious  * Double.pi;
+            }
+            
+        }
+
+        return area;
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+fileprivate class shape:UIView
+{
+    var whichShape:String!
+    var width:Double!
+    var height:Double!
+    var radious:Double!
+    func area()->Double
+    {
+        return 0.0
+        
+    }
+    
+}
+
